650433981e9f5720aa67cb81a20b19f386a130cd
Rafael Freitas <rafael_cssa@hotmail.com>
2021-06-09 22:00:37 -0300

    Heranca e polimorfismo 01

---
+++ src/HerancaPolimorfismo/Heranca01.java
@@ -0,0 +1,15 @@
+package HerancaPolimorfismo;
+
+import entities.BusinessAccount;
+
+public class Heranca01 {
+
+	public static void main(String[] args) {
+		
+		BusinessAccount account = new BusinessAccount();
+		
+		account.getBalance();
+
+	}
+
+}
