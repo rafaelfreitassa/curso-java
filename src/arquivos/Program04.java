af148d4436b111c8b52e6fa5d69e49296c20ecb5
Rafael Freitas <rafael_cssa@hotmail.com>
2021-09-23 21:37:09 -0300

    Arquivos: FileWriter e BufferedWriter

---
+++ src/arquivos/Program04.java
@@ -0,0 +1,27 @@
+package arquivos;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program04 {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
+		
+		String path = "c:\\temp\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path/*, true //altera o arquivo*/))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
