5590a877e643f521fb48c1473e463cc94812b85f
Rafael Freitas <rafael_cssa@hotmail.com>
2021-10-23 15:47:27 -0300

    Generics, Set, Map: Curingas delimitados Parte 02

---
+++ src/genericsSetMap/Program4.java
@@ -0,0 +1,56 @@
+package genericsSetMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		
+		/*List<Integer> intList = new ArrayList<Integer>();
+		intList.add(10);
+		intList.add(5);
+		
+		List<? extends Number> list = intList;
+		
+		Number x = list.get(0);
+		
+		list.add(20);
+		
+		List<Object> myObjs = new ArrayList<>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
+		
+		List<? super Number> myNums = myObjs;
+		
+		myNums.add(10);
+		myNums.add(3.14);
+		
+		Number x = myNums.get(0);*/
+		
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
+		
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+
+	}
+	
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
+		}
+	}
+	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
+	}
+
+}
