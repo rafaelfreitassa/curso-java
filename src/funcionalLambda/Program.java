9574102279b64ddf466d17e47a6ee746c76caa41
Rafael Freitas <rafael_cssa@hotmail.com>
2021-11-18 20:23:18 -0300

    Progamacao funcional e expressoes lambda: Parte 03

--- src/funcionalLambda/Program.java
+++ src/funcionalLambda/Program.java
@@ -1,6 +1,7 @@
 package funcionalLambda;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product8;
@@ -15,7 +16,16 @@ public class Program {
 		list.add(new Product8("Notebook", 1200.00));
 		list.add(new Product8("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+		Comparator<Product8> comp = new Comparator<Product8>() {
+
+			@Override
+			public int compare(Product8 p1, Product8 p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+			
+		};
+		
+		list.sort(comp);
 		
 		for (Product8 p : list) {
 			System.out.println(p);
