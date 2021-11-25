bfd09e06157d0e924a806e9ad6703e82ace15039
Rafael Freitas <rafael_cssa@hotmail.com>
2021-11-25 20:06:28 -0300

    Progamacao funcional e expressoes lambda: Predicate parte 02

--- src/funcionalLambda/Program03.java
+++ src/funcionalLambda/Program03.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Product8;
+import util.ProductPredicate;
 
 public class Program03 {
 
@@ -16,7 +17,7 @@ public class Program03 {
 		list.add(new Product8("Tablet", 350.00));
 		list.add(new Product8("Notebook", 80.90));
 				
-		list.removeIf(p -> p.getPrice() >= 100);
+		list.removeIf(new ProductPredicate());
 		
 		for (Product8 p : list) {
 			System.out.println(p);
