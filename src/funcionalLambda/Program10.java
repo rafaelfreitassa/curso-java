aed330fbf930dfccdb14fd5d95de990a9b0a0e9d
Rafael Freitas <rafael_cssa@hotmail.com>
2021-12-27 20:11:16 -0300

    Progamacao funcional e expressoes lambda: Exercicio fixacao

---
+++ src/funcionalLambda/Program10.java
@@ -0,0 +1,66 @@
+package funcionalLambda;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employee3;
+import entities.Product8;
+
+public class Program10 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		System.out.print("Enter salary: ");
+		double salary = sc.nextDouble();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			List<Employee3> list = new ArrayList<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Employee3(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+			}
+			
+			List<String> names = list.stream()
+					.filter(p -> p.getSalary() > salary)
+					.map(p -> p.getEmail())
+					.sorted()
+					.collect(Collectors.toList());
+			
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary) + ":");
+			names.forEach(System.out::println);
+			
+			double sum = list.stream()
+					.filter(p -> p.getName().charAt(0) == 'M')
+					.map(p -> p.getSalary())
+					.reduce(0.0, (x,y) -> x + y);
+			
+			System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
+
+	}
+
+}
